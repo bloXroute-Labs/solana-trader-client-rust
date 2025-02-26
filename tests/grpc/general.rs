86810db5ee7c68c043659ccf4fd520aaaff0915d
Lanikean <jose.bravo@bloxroute.com>
2025-02-26 07:53:55 -0700

    refactor general -> mod

--- tests/grpc/general.rs
+++ tests/grpc/general.rs
@@ -3,11 +3,14 @@ use std::str::FromStr;
 use anyhow::Result;
 use base64::{engine::general_purpose, Engine};
 use solana_hash::Hash;
-use solana_sdk::{pubkey::Pubkey, system_instruction};
+use solana_sdk::{
+    pubkey::Pubkey, signature::Signature, signer::Signer as _, system_instruction,
+    transaction::Transaction,
+};
 use solana_trader_client_rust::{
     common::{
         constants::{SAMPLE_OWNER_ADDR, SAMPLE_TX_SIGNATURE},
-        signing::create_signed_transaction,
+        signing::{create_signed_transaction, SubmitParams},
     },
     provider::grpc::GrpcClient,
 };
@@ -243,6 +246,65 @@ async fn test_submit_snipe() -> anyhow::Result<()> {
 
     let signatures = client.sign_and_submit_snipe(transactions, true).await?;
     println!("Snipe Signatures: {signatures:?}");
+
+    Ok(())
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_paladin_race() -> anyhow::Result<()> {
+    let mut handles = vec![];
+
+    for _ in 0..100 {
+        let handle = tokio::spawn(async move {
+            // Create new client for each task
+            let mut client = GrpcClient::new(None).await?;
+
+            // Similar to test_add_memo_to_tx but with Paladin params
+            let block_hash = client
+                .get_recent_block_hash_v2(GetRecentBlockHashRequestV2 { offset: 0 })
+                .await?
+                .block_hash
+                .parse::<Hash>()?;
+
+            let lamports_to_transfer = 1_000_000;
+            let pubkey = client.public_key.unwrap();
+            let keypair = client.get_keypair()?;
+
+            let transfer_instruction =
+                system_instruction::transfer(&pubkey, &pubkey, lamports_to_transfer);
+
+            let mut transaction = Transaction::new_signed_with_payer(
+                &[transfer_instruction],
+                Some(&pubkey),
+                &[&keypair],
+                block_hash,
+            );
+
+            let message_data = transaction.message.serialize();
+            transaction.signatures = vec![Signature::default()];
+            transaction.signatures[0] = keypair.sign_message(&message_data);
+
+            let serialized_tx = bincode::serialize(&transaction)?;
+            let messages = vec![TransactionMessage {
+                content: general_purpose::STANDARD.encode(serialized_tx),
+                is_cleanup: false,
+            }];
+
+            let submit_opts = SubmitParams {
+                allow_revert: Some(true), // Trigger Paladin path
+                ..Default::default()
+            };
+
+            client.sign_and_submit(messages, submit_opts, false).await
+        });
+        handles.push(handle);
+    }
+
+    // Wait for all transactions
+    for handle in handles {
+        handle.await??;
+    }
 
     Ok(())
 }
